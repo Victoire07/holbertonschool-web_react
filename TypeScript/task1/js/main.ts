28f866554132c7ac7e0e734c59c5f995127caba8
Victoire07 <victoireboutin07@gmail.com>
2025-09-11 18:59:03 +0200

    ajout de l'interrafce Teacher et d el'objet qui en découle

---
+++ TypeScript/task1/js/main.ts
@@ -0,0 +1,18 @@
+// Création de l'interface
+interface Teacher {
+    firstName: string;
+    lastName: string
+    fullTimeEmployee: boolean
+    yearsOfExperience?: number
+    location: string
+    [key: string]: any; // Autoriser l'ajout de propriétés dynamiques
+}
+
+// Création d'un objet 
+const teacher3: Teacher = {
+  firstName: "Catherine",
+  lastName: "Chevalier",
+  fullTimeEmployee: false,
+  location: "Dijon",
+  contract: false,
+};
