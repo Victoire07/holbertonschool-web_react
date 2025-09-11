54d3f2ebb3cb5bff01d58700ad61abdf613a3aa1
Victoire07 <victoireboutin07@gmail.com>
2025-09-11 18:01:28 +0200

    ajout tableau HTML et remplir avec les données des élèves

--- TypeScript/task0/js/main.ts
+++ TypeScript/task0/js/main.ts
@@ -1,5 +1,5 @@
 // Création de l'interface
-interface Students {
+interface Student {
     firstName: string;
     lastName: string
     age: number
@@ -7,14 +7,14 @@ interface Students {
 }
 
 // Ajout des 2 étudiants
-const student1: Students = {
+const student1: Student = {
   firstName: "Victoire",
   lastName: "Boutin",
   age: 25,
   location: "Bordeaux"
 };
 
-const student2: Students = {
+const student2: Student = {
   firstName: "Souris",
   lastName: "Dufau",
   age: 26,
@@ -22,6 +22,27 @@ const student2: Students = {
 };
 
 // Stockage des 2 étudiants dans une liste
-const studentsList: Students[] = [student1, student2];
+const studentsList: Student[] = [student1, student2];
 
+// Creation table
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
 
+// Remplir le tableau avec les données des élèves
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+
+  row.appendChild(firstNameCell);
+  row.appendChild(locationCell);
+  tbody.appendChild(row);
+});
+
+// Ajouter le tableau à la page
+table.appendChild(tbody);
+document.body.appendChild(table);
