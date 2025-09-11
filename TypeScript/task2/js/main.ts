c4c66fc99b9c2e110b80455314fd825419ab3e99
Victoire07 <victoireboutin07@gmail.com>
2025-09-12 01:19:12 +0200

    ajout des 2 fonctions pour savoir quels types de tâches selon le poste

--- TypeScript/task2/js/main.ts
+++ TypeScript/task2/js/main.ts
@@ -55,3 +55,19 @@ console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
 
+// Fonction isDirector
+function isDirector(employee: Employee): employee is Director {
+    return employee instanceof Director
+}
+
+// Fonction executeWork
+function executeWork(employee: Employee): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
