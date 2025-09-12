04a8c865cbb129158e156c631ef664a243004bb3
Victoire07 <victoireboutin07@gmail.com>
2025-09-12 11:48:22 +0200

    MàJ fichier

--- TypeScript/task4/js/subjects/Java.ts
+++ TypeScript/task4/js/subjects/Java.ts
@@ -11,7 +11,11 @@ namespace Subjects {
     getRequirements(): string {
       return "Here is the list of requirements for Java";
     }
-
-    
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return "No available teacher";
+    }
   }
 }
