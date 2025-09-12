8273f076f19680c903bd024e347133c96f487fb6
Victoire07 <victoireboutin07@gmail.com>
2025-09-12 13:07:42 +0200

    IDK, STOP

--- TypeScript/task4/js/subjects/React.ts
+++ TypeScript/task4/js/subjects/React.ts
@@ -2,19 +2,17 @@
 /// <reference path="./Subject.ts" />
 
 namespace Subjects {
-  // Declaration merging: on ajoute une propriété optionnelle
   export interface Teacher {
     experienceTeachingReact?: number;
   }
 
-  // Classe React qui hérite de Subject
   export class React extends Subject {
     getRequirements(): string {
       return "Here is the list of requirements for React";
     }
 
     getAvailableTeacher(): string {
-      if (this.teacher && this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+      if (this.teacher?.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
         return `Available Teacher: ${this.teacher.firstName}`;
       }
       return "No available teacher";
