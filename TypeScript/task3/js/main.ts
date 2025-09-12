854be36613541737f1214765f8fa7a4effe92613
Victoire07 <victoireboutin07@gmail.com>
2025-09-12 10:15:02 +0200

    Ajout des appels CRUD

--- TypeScript/task3/js/main.ts
+++ TypeScript/task3/js/main.ts
@@ -1,2 +1,23 @@
 // Triple slash directive qui inclut 'crud.d.ts'
 /// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from "./interface";
+import * as CRUD from './crud';
+
+// Création d'un objet RowElement
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva"
+};
+
+// Insertion row
+const newRowID: RowID = CRUD.insertRow(row)
+
+// Update row
+const updatedRow: RowElement = {
+    ...row, age: 23
+};
+
+// Appeller CRUD updateRow et CRUD deleteRow
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
